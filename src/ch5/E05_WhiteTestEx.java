34fb008d750cce67a56c50dfca3e4dcc58161087
qmx <1639296181@qq.com>
2020-09-28 14:47:08 +0800

    5.3 ex

--- src/ch5/E05_WhiteTestEx.java
+++ src/ch5/E05_WhiteTestEx.java
@@ -5,6 +5,8 @@ public class E05_WhiteTestEx {
 	public static int sum(int N, int Max) throws TooLargeException {
 		int sum = 0 ;
 		int k = 0;
+		if(N<0) N = -1*N;
+		//TDD,如果不能写出让测试失败的用例，那么就没有必要增加逻辑代码
 		while(k<N) {//无法驱动出 && sum<=Max
 			sum += k;
 			k++;
--- src/ch5/E05_WhiteTestExXPTest.java
+++ src/ch5/E05_WhiteTestExXPTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 class E05_WhiteTestExXPTest {
 	@ParameterizedTest(name = "when N is {0}, Max is {1}, expect res is {2}")
 	@CsvSource({
-	    "0,10,0", "4, 10,6"
+	    "0,10,0", "4, 10,6", "-4, 10, 6"
 	})
 	void should_return_sum_when_sum_is_smaller_than_max(int n, int max, int expRes) throws TooLargeException {
 		int res = E05_WhiteTestEx.sum(n, max);
@@ -23,7 +23,7 @@ class E05_WhiteTestExXPTest {
 	
 	@Test
 	void should_throw_ErrorMes_when_res_is_bigger_than_Max_version2() {
-		Throwable throwable = assertThrows(TooLargeException.class, ()->E05_WhiteTestEx.sum(0, -1));
+		Throwable throwable = assertThrows(TooLargeException.class, ()->E05_WhiteTestEx.sum(0,-1));
 		assertAll(() -> assertEquals("Error: too large", throwable.getMessage()));
 	}
     
