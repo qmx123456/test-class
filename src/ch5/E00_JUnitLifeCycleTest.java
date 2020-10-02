c7b921a8f3f1dfcc5a2ba4608f3c43ce4ec6bf45
qmx <1639296181@qq.com>
2020-10-02 09:21:11 +0800

    junit life cycle

---
+++ src/ch5/E00_JUnitLifeCycleTest.java
@@ -0,0 +1,65 @@
+package ch5;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class E00_JUnitLifeCycleTest {
+	@BeforeAll
+	static void beforeAll1() {
+		System.out.println("BeforeAll 1");
+	}
+	@BeforeAll
+	static void beforeAll2() {
+		System.out.println("BeforeAll 2");
+	}
+	@BeforeEach
+	void beforeEach1() {
+		System.out.println("BeforeEach 1");
+	}
+	@BeforeEach
+	void beforeEach2() {
+		System.out.println("BeforeEach 2");
+	}
+	@Test
+	void test1() {
+		System.out.println("Test 1");		
+	}
+	@Test
+	void test2() {
+		System.out.println("Test 2");		
+	}
+	@BeforeEach
+	void beforeEach3() {
+		System.out.println("BeforeEach 3");
+	}
+	
+	@BeforeAll
+	static void beforeAll3() {
+		System.out.println("BeforeAll 3");
+	}
+	
+	@AfterAll
+	static void AfterAll1() {
+		System.out.println("AfterAll 1");
+	}
+	
+	@AfterAll
+	static void AfterAll2() {
+		System.out.println("AfterAll 2");
+	}
+		
+	@AfterEach
+	void AfterEach1() {
+		System.out.println("AfterEach 1");
+	}
+	
+	@AfterEach
+	void AfterEach2() {
+		System.out.println("AfterEach 2");
+	}
+	
+	
+}
