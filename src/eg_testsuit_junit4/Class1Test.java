09926b99298aae6876ac64ae83c5f1ce521bf6b7
qmx <1639296181@qq.com>
2020-10-14 21:03:40 +0800

    test suite using junit4

---
+++ src/eg_testsuit_junit4/Class1Test.java
@@ -0,0 +1,24 @@
+package eg_testsuit_junit4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Class1Test {
+
+	@Test
+	public void test1() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test2() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test3() {
+		fail("Not yet implemented");
+	}
+
+}
