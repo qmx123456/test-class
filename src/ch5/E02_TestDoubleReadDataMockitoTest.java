9f60725c0f5ac9398d3dfe6c4b2e8e74c12d3bdd
qmx <1639296181@qq.com>
2020-10-16 00:16:55 +0800

    spy for mockito

--- src/ch5/E02_TestDoubleReadDataMockitoTest.java
+++ src/ch5/E02_TestDoubleReadDataMockitoTest.java
@@ -3,6 +3,8 @@ package ch5;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 import static org.mockito.Mockito.*;
 
 import java.util.LinkedList;
@@ -27,4 +29,25 @@ class E02_TestDoubleReadDataMockitoTest {
 		verify(mockHandle).deal(setRes);
 	}
 	//TODO spy, mock, dummy, fake, stub
+	
+	@Test
+	void testSyp() {
+		String fileName = "fileName";
+		String[] setRes = new String[] {"12"};
+		Data data = new Data();
+		
+		FileSystem storeSystem = new FileSystem();
+		FileSystem spyStore = Mockito.spy(storeSystem);//浅复制，会多记录调用次数等调用信息
+		doReturn(setRes).when(spyStore).read(fileName);
+		
+		SingleDataHandle spyHandle = Mockito.spy(SingleDataHandle.class);
+		doReturn(data).when(spyHandle).deal(setRes);
+		
+		Data res = E02_TestDoubleReadData.read(spyStore, spyHandle, fileName);		
+		assertEquals(data, res);
+		verify(spyStore).read(fileName);
+		verify(spyHandle).deal(setRes);
+		
+		verify(spyHandle,times(1)).deal(setRes);
+	}
 }
